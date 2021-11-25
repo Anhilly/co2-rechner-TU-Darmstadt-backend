d154608f9b4d9f3987214799bc95f19b8466d9b9
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-11-25 11:06:07 +0100

    feat(db_structs): added Energieversorgung struct

--- database/db_structs.go
+++ database/db_structs.go
@@ -1,1 +1,13 @@
 package database
+
+type Energieversorgung struct {
+	Kategorie string `json:"kategorie" bson:"kategorie"`
+	Einheit string `json:"einheit" bson:"einheit"`
+	Revision int32 `json:"revision" bson:"revision"`
+	CO2Faktor []CO2Energie `json:"CO2Faktor" bson:"CO2Faktor"`
+}
+
+type CO2Energie struct{
+	Wert float64 `json:"wert" bson:"wert"`
+	Jahr int32 `json:"jahr" bson:"jahr"`
+}
