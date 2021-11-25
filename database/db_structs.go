5d44a16d1a5f64e8511e2563fd8a25154fcbc873
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-11-25 11:26:08 +0100

    feat(db_structs): added structs for ITGeraete, Dienstreisen & Pendelweg
    These structs represent the Collections in our Database

--- database/db_structs.go
+++ database/db_structs.go
@@ -1,5 +1,6 @@
 package database
 
+//Collection energieversorgung
 type Energieversorgung struct {
 	Kategorie string `json:"kategorie" bson:"kategorie"`
 	Einheit string `json:"einheit" bson:"einheit"`
@@ -10,4 +11,35 @@ type Energieversorgung struct {
 type CO2Energie struct{
 	Wert float64 `json:"wert" bson:"wert"`
 	Jahr int32 `json:"jahr" bson:"jahr"`
+}
+
+//Collection itGeraete
+type ITGeraete struct {
+	Kategorie string `json:"kategorie" bson:"kategorie"`
+	CO2FaktorGesamt int32 `json:"CO2FaktorGesamt" bson:"CO2FaktorGesamt"`
+	CO2FaktorJahr int32 `json:"CO2FaktorJahr" bson:"CO2FaktorJahr"`
+	Einheit string `json:"einheit" bson:"einheit"`
+	Revision int32 `json:"revision" bson:"revision"`
+}
+
+//Collection dienstreisen
+type Dienstreisen struct {
+	Medium string `json:"medium" bson:"medium"`
+	Einheit string `json:"einheit" bson:"einheit"`
+	Revision int32 `json:"revision" bson:"revision"`
+	CO2Faktor []CO2Dienstreisen `json:"CO2Faktor" bson:"CO2Faktor"`
+}
+
+type CO2Dienstreisen struct {
+	Tankart string `json:"Tankart" bson:"Tankart"`
+	Strecke int32 `json:"Strecke" bson:"Strecke"`
+	Wert int32 `json:"Wert" bson:"Wert"`
+}
+
+//Collection pendelweg
+type Pendelweg struct {
+	Medium string `json:"medium" bson:"medium"`
+	CO2Faktor int32 `json:"CO2Faktor" bson:"CO2Faktor"`
+	Einheit int32 `json:"einheit" bson:"einheit"`
+	Revision int32 `json:"revision" bson:"revision"`
 }
