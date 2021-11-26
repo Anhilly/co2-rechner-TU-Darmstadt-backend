f306ec224e1cd58f745b97b10495877411bf0239
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-11-26 20:23:21 +0100

    feat(server): Added StartServer

--- server/server.go
+++ server/server.go
@@ -1,1 +1,28 @@
 package server
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"log"
+	"net/http"
+)
+
+func StartServer() {
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	log.Fatalln(http.ListenAndServe(":9000", r))
+}
