7b50df612e07996fe466f19d4d18eda1707c067c
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-11-26 20:12:12 +0100

    feat(database): Added ConnectDatabase & DisconnectDatabase

--- database/database.go
+++ database/database.go
@@ -1,1 +1,46 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
+
+var client *mongo.Client
+
+func ConnectDatabase() {
+	var err error
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	fmt.Println("Connect")
+
+	client, err = mongo.NewClient(options.Client().ApplyURI("mongodb://" + username + ":" + password + "@" + serverAdress + ":" + port + "/" + dbName))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connect successful")
+}
+
+func DisconnectDatabase() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	fmt.Println("Disconnect")
+	err := client.Disconnect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
