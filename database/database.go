6ddd1e1e63aa2c7035109eb5a5c7605d5b2333ee
Tobias512 <tobi-wehr@web.de>
2021-11-27 14:42:13 +0100

    refactor: added comments to database.go and functions now have return type error

--- database/database.go
+++ database/database.go
@@ -2,45 +2,50 @@ package database
 
 import (
 	"context"
-	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"log"
 	"time"
 )
 
 var client *mongo.Client
 
-func ConnectDatabase() {
+/**
+Die Funktion stellt eine Verbindung mit der Datenbank her mittels der Konstanten aus db_config.go
+Die Referenz zur Datenbank wird in der Variable client gepseichert
+*/
+func ConnectDatabase() error {
 	var err error
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 
-	fmt.Println("Connect")
-
 	client, err = mongo.NewClient(options.Client().ApplyURI("mongodb://" + username + ":" + password + "@" + serverAdress + ":" + port + "/" + dbName))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = client.Connect(ctx)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	fmt.Println("Connect successful")
+
+	return nil
 }
 
-func DisconnectDatabase() {
+/**
+Die Funktion schließt die Verbindung mit der Datenbank.
+*/
+func DisconnectDatabase() error {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-	fmt.Println("Disconnect")
 	err := client.Disconnect(ctx)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
