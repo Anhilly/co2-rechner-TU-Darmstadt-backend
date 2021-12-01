c7ed4041946a9eb403542e1f03bf23142cd87909
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-12-01 08:15:35 +0100

    feat(gebaeude): Added GebaeudeFind method

--- database/gebaeude.go
+++ database/gebaeude.go
@@ -1,6 +1,34 @@
 package database
 
-//temporärer Placeholder
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"time"
+)
+
+const (
+	gebaeudeCol = "gebaeude"
+)
+
+/**
+Die Funktion liefert einen Gebaeude struct mit nr gleich dem Parameter.
+*/
 func GebaeudeFind(nr int32) (Gebaeude, error) {
-	return Gebaeude{}, nil
+	var data Gebaeude
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	collection := client.Database(dbName).Collection(gebaeudeCol)
+
+	cursor, err := collection.Find(ctx, bson.D{{"nr", nr}})
+	if err != nil {
+		return Gebaeude{}, err
+	}
+
+	cursor.Next(ctx)
+	err = cursor.Decode(&data)
+	if err != nil {
+		return Gebaeude{}, err
+	}
+
+	return data, nil
 }
