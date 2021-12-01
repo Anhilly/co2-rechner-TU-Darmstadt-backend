0649d55f7c01204230b87605336e44ff7d4343cb
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-12-01 08:24:39 +0100

    feat(wearmezaehler): Added wearmezaehlerFind Method

--- database/waermezaehler.go
+++ database/waermezaehler.go
@@ -1,6 +1,35 @@
 package database
 
-//temporärer Placeholder
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"time"
+)
+
+const (
+	waermezaehlerCol = "waermezaehler"
+)
+
+
+/**
+Die Funktion liefert einen Waermezaehler struct mit pkEnergie gleich dem Parameter.
+*/
 func WaermezaehlerFind(pkEnergie int32) (Waermezaehler, error) {
-	return Waermezaehler{}, nil
+	var data Waermezaehler
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	collection := client.Database(dbName).Collection(waermezaehlerCol)
+
+	cursor, err := collection.Find(ctx, bson.D{{"pkEnergie", pkEnergie}})
+	if err != nil {
+		return Waermezaehler{}, err
+	}
+
+	cursor.Next(ctx)
+	err = cursor.Decode(&data)
+	if err != nil {
+		return Waermezaehler{}, err
+	}
+
+	return data, nil
 }
