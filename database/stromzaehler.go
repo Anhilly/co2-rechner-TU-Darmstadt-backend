66e73955126484efcc57b5d1322a8bd1d77a2b3f
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-12-01 08:21:17 +0100

    feat(stromzaehler): Added stromzaehlerFind Method

--- database/stromzaehler.go
+++ database/stromzaehler.go
@@ -1,6 +1,33 @@
 package database
 
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"time"
+)
+
+const (
+	stromzaehlerCol = "stromzaehler"
+)
+
+
 //temporärer Placeholder
 func StromzaehlerFind(pkEnergie int32) (Stromzaehler, error) {
-	return Stromzaehler{}, nil
+	var data Stromzaehler
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	collection := client.Database(dbName).Collection(stromzaehlerCol)
+
+	cursor, err := collection.Find(ctx, bson.D{{"pkEnergie", pkEnergie}})
+	if err != nil {
+		return Stromzaehler{}, err
+	}
+
+	cursor.Next(ctx)
+	err = cursor.Decode(&data)
+	if err != nil {
+		return Stromzaehler{}, err
+	}
+
+	return data, nil
 }
