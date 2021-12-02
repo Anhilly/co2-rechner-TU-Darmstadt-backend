ea9a8a5115b077597a55742ea574e9beb86a4d9c
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-12-02 11:50:33 +0100

    feat(tbFind_test): Added test for KaeltedatenFind()

--- tests/dbFind_test.go
+++ tests/dbFind_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Anhilly/co2-rechner-TU-Darmstadt-backend/database"
 	"github.com/matryer/is"
 	"testing"
+	"time"
 )
 
 func TestFind(t *testing.T) {
@@ -27,7 +28,7 @@ func TestITGeraeteFind(t *testing.T) {
 		data, err := database.ITGeraeteFind(1)
 
 		is.NoErr(err)                                                                                                                                               // Error seitens der Datenbank
-		is.Equal(data, database.ITGeraete{IDITGerate: 1, Kategorie: "Notebooks", CO2FaktorGesamt: 588000, CO2FaktorJahr: 147000, Einheit: "g/Stueck", Revision: 1}) // Überprüfung des zurrückgelieferten Elements
+		is.Equal(data, database.ITGeraete{IDITGerate: 1, Kategorie: "Notebooks", CO2FaktorGesamt: 588000, CO2FaktorJahr: 147000, Einheit: "g/Stueck", Revision: 1}) // Überprüfung des zurückgelieferten Elements
 	})
 
 }
@@ -41,7 +42,7 @@ func TestDienstreisenFind(t *testing.T) {
 		data, err := database.DienstreisenFind(1)
 
 		is.NoErr(err)                                                                                                                                             // Error seitens der Datenbank
-		is.Equal(data, database.Dienstreisen{IDDienstreisen: 1, Medium: "Bahn", Einheit: "g/Pkm", Revision: 1, CO2Faktor: []database.CO2Dienstreisen{{Wert: 8}}}) // Überprüfung des zurrückgelieferten Elements
+		is.Equal(data, database.Dienstreisen{IDDienstreisen: 1, Medium: "Bahn", Einheit: "g/Pkm", Revision: 1, CO2Faktor: []database.CO2Dienstreisen{{Wert: 8}}}) // Überprüfung des zurückgelieferten Elements
 	})
 }
 
@@ -53,13 +54,13 @@ func TestEnergieversorgungFind(t *testing.T) {
 
 		data, err := database.EnergieversorgungFind(1)
 
-		is.NoErr(err)                                                                                                                                                                         // Error seitens der Datenbank
+		is.NoErr(err) // Error seitens der Datenbank
 		is.Equal(data, database.Energieversorgung{
 			IDEnergieversorgung: 1,
-			Kategorie: "Fernwaerme",
-			Einheit: "g/kWh",
-			Revision: 1,
-			CO2Faktor: []database.CO2Energie{{Wert: 144, Jahr: 2020}}}) // Überprüfung des zurrückgelieferten Elements
+			Kategorie:           "Fernwaerme",
+			Einheit:             "g/kWh",
+			Revision:            1,
+			CO2Faktor:           []database.CO2Energie{{Wert: 144, Jahr: 2020}}}) // Überprüfung des zurückgelieferten Elements
 	})
 }
 
@@ -89,6 +90,40 @@ func TestGebaeudeFind(t *testing.T) {
 			Revision:    1,
 			KaelteRef:   []int32{},
 			WaermeRef:   []int32{2084},
-			StromRef:    []int32{}}) // Überprüfung des zurrückgelieferten Elements
+			StromRef:    []int32{}}) // Überprüfung des zurückgelieferten Elements
+	})
+}
+
+func TestKaeltezaehlerFind(t *testing.T) {
+	is := is.NewRelaxed(t)
+
+	t.Run("KaelteFind: ID = 1", func(t *testing.T) {
+		is := is.NewRelaxed(t)
+
+		data, err := database.KaeltezaehlerFind(4023)
+		location, err := time.LoadLocation("Etc/GMT")
+
+		is.NoErr(err) // Error seitens der Datenbank
+		is.Equal(data, database.Zaehler{Zaehlertyp: "Kaelte",
+			PKEnergie:   4023,
+			Bezeichnung: "3101 Kaelte Hauptzaehler",
+			Zaehlerdaten: []database.Zaehlerwerte{
+				{
+					Wert:        311.06,
+					Zeitstempel: time.Date(2021, time.January, 01, 0, 0, 0, 0, location),
+				},
+				{
+					Wert:        414.61,
+					Zeitstempel: time.Date(2020, time.January, 01, 0, 0, 0, 0, location),
+				},
+				{
+					Wert:        555.3,
+					Zeitstempel: time.Date(2019, time.January, 01, 0, 0, 0, 0, location),
+				},
+				{
+					Wert:        169.59,
+					Zeitstempel: time.Date(2018, time.January, 01, 0, 0, 0, 0, location),
+				},
+			}})
 	})
 }
