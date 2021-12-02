c5aee5fbd9fd548ec6bec4382088bc7ea3d52e97
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-12-02 11:24:17 +0100

    feat(tbFind_test): Added test for GebaeudeFind()

--- tests/dbFind_test.go
+++ tests/dbFind_test.go
@@ -13,8 +13,9 @@ func TestFind(t *testing.T) {
 	defer database.DisconnectDatabase()
 
 	t.Run("TestITGeareteFind", TestITGeraeteFind)
-	t.Run("TestITEnergieversorgungFind", TestEnergieversorgungFind)
-	t.Run("TestITDienstreisenFind", TestDienstreisenFind)
+	t.Run("TestEnergieversorgungFind", TestEnergieversorgungFind)
+	t.Run("TestDienstreisenFind", TestDienstreisenFind)
+	t.Run("TestGebaeudeFind", TestGebaeudeFind)
 }
 
 func TestITGeraeteFind(t *testing.T) {
@@ -31,7 +32,7 @@ func TestITGeraeteFind(t *testing.T) {
 
 }
 
-func TestDienstreisenFind(t *testing.T){
+func TestDienstreisenFind(t *testing.T) {
 	is := is.NewRelaxed(t)
 
 	t.Run("DienstreisenFind: ID = 1", func(t *testing.T) {
@@ -39,7 +40,7 @@ func TestDienstreisenFind(t *testing.T){
 
 		data, err := database.DienstreisenFind(1)
 
-		is.NoErr(err)                                                                                                                                               // Error seitens der Datenbank
+		is.NoErr(err)                                                                                                                                             // Error seitens der Datenbank
 		is.Equal(data, database.Dienstreisen{IDDienstreisen: 1, Medium: "Bahn", Einheit: "g/Pkm", Revision: 1, CO2Faktor: []database.CO2Dienstreisen{{Wert: 8}}}) // Überprüfung des zurrückgelieferten Elements
 	})
 }
@@ -52,7 +53,42 @@ func TestEnergieversorgungFind(t *testing.T) {
 
 		data, err := database.EnergieversorgungFind(1)
 
-		is.NoErr(err)                                                                                                                                               // Error seitens der Datenbank
-		is.Equal(data, database.Energieversorgung{IDEnergieversorgung: 1, Kategorie: "Fernwaerme", Einheit: "g/kWh", Revision: 1, CO2Faktor: []database.CO2Energie{{Wert: 144, Jahr: 2020}}}) // Überprüfung des zurrückgelieferten Elements
+		is.NoErr(err)                                                                                                                                                                         // Error seitens der Datenbank
+		is.Equal(data, database.Energieversorgung{
+			IDEnergieversorgung: 1,
+			Kategorie: "Fernwaerme",
+			Einheit: "g/kWh",
+			Revision: 1,
+			CO2Faktor: []database.CO2Energie{{Wert: 144, Jahr: 2020}}}) // Überprüfung des zurrückgelieferten Elements
+	})
+}
+
+func TestGebaeudeFind(t *testing.T) {
+	is := is.NewRelaxed(t)
+
+	t.Run("GebaeudeFind: ID = 1", func(t *testing.T) {
+		is := is.NewRelaxed(t)
+
+		data, err := database.GebaeudeFind(1102)
+
+		is.NoErr(err) // Error seitens der Datenbank
+		is.Equal(data, database.Gebaeude{
+			Nr:          1101,
+			Bezeichnung: "Universitaetszentrum, karo 5, Audimax",
+			Flaeche: database.GebaeudeFlaeche{
+				HNF:     6395.56,
+				NNF:     3081.85,
+				NGF:     15365.03,
+				FF:      5539.21,
+				VF:      5887.62,
+				FreiF:   96.57,
+				GesamtF: 21000.81,
+			},
+			Einheit:     "m^2",
+			Spezialfall: 1,
+			Revision:    1,
+			KaelteRef:   []int32{},
+			WaermeRef:   []int32{2084},
+			StromRef:    []int32{}}) // Überprüfung des zurrückgelieferten Elements
 	})
 }
