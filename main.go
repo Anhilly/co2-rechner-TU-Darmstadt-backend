edab6d07df0417610667ee981e57a989650b4054
Tobias512 <tobi-wehr@web.de>
2021-12-02 10:47:02 +0100

    feat(dbFind_test): added first test for database.ITGeareteFind()

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import "github.com/Anhilly/co2-rechner-TU-Darmstadt-backend/importer"
+import (
+	"fmt"
+	"github.com/Anhilly/co2-rechner-TU-Darmstadt-backend/database"
+)
 
 func main() {
 	//importer.ImportEnergieversorgung()
-	importer.ImportGebaeude()
-	importer.ImportStromzaehler()
-	importer.ImportWaermedaten()
-	importer.ImportKaeltedaten()
+	//importer.ImportGebaeude()
+	//importer.ImportStromzaehler()
+	//importer.ImportWaermedaten()
+	//importer.ImportKaeltedaten()
+
+	database.ConnectDatabase()
+
+	data, err := database.ITGeraeteFind(100)
+
+	fmt.Println(err)
+	fmt.Println(data)
+
+	database.DisconnectDatabase()
 }
--- tests/dbFind_test.go
+++ tests/dbFind_test.go
@@ -1,1 +1,31 @@
 package tests
+
+import (
+	"github.com/Anhilly/co2-rechner-TU-Darmstadt-backend/database"
+	"github.com/matryer/is"
+	"testing"
+)
+
+func TestFind(t *testing.T) {
+	//is := is.NewRelaxed(t)
+
+	database.ConnectDatabase()
+	defer database.DisconnectDatabase()
+
+	t.Run("TestITGeareteFind", TestITGeraeteFind)
+
+}
+
+func TestITGeraeteFind(t *testing.T) {
+	is := is.NewRelaxed(t)
+
+	t.Run("Test ID = 1", func(t *testing.T) {
+		is := is.NewRelaxed(t)
+
+		data, err := database.ITGeraeteFind(1)
+
+		is.NoErr(err)                                                                                                                                               // Error seitens der Datenbank
+		is.Equal(data, database.ITGeraete{IDITGerate: 1, Kategorie: "Notebooks", CO2FaktorGesamt: 588000, CO2FaktorJahr: 147000, Einheit: "g/Stueck", Revision: 1}) // Überprüfung des zurrückgelieferten Elements
+	})
+
+}
