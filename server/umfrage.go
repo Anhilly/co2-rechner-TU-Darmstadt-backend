1ec4faf0a1a110dbcb5874221a85cdda0ab8463c
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-12-02 15:22:32 +0100

    feat(umfrage): Added PostMitarbeiter

--- server/umfrage.go
+++ server/umfrage.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"encoding/json"
+	"github.com/Anhilly/co2-rechner-TU-Darmstadt-backend/co2computation"
 	"github.com/go-chi/chi/v5"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -14,5 +17,16 @@ func RouteUmfrage() chi.Router{
 }
 
 func PostMitarbeiter(res http.ResponseWriter , req *http.Request){
-	
+	s, _ := ioutil.ReadAll(req.Body)
+	umfrageReq := UmfrageMitarbeiterReq{}
+	umfrageRes := UmfrageMitarbeiterRes{}
+	json.Unmarshal(s, &umfrageReq)
+	umfrageRes.DienstreisenEmissionen, _ = co2computation.BerechneDienstreisen(umfrageReq.Dienstreise)
+	umfrageRes.PendelwegeEmissionen, _ = co2computation.BerechnePendelweg(umfrageReq.Pendelweg, umfrageReq.TageImBuero)
+	umfrageRes.ITGeraeteEmissionen, _ = co2computation.BerechneITGeraete(umfrageReq.ITGeraete)
+
+	response, _ := json.Marshal(umfrageRes)
+
+	res.WriteHeader(http.StatusOK)
+	res.Write(response)
 }
