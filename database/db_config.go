88bd3b563e2e71d523647ee9c8142d95172a6775
Tobias512 <tobi-wehr@web.de>
2021-12-10 13:14:31 +0100

    refactor: only commits for dump.go

---
+++ database/db_config.go
@@ -0,0 +1,11 @@
+package database
+
+const (
+	serverAdress = "localhost"
+	port         = "27017"
+	username     = "testUser"
+	// username = "root"
+	password = "testUser"
+	// password = "test_pw"
+	dbName = "co2Rechner"
+)
