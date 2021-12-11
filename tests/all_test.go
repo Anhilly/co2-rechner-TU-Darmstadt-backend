bf29a3620a47cb3c267231e47b10b06b7e6635d2
Lithium-1Hauptgruppe <62305832+Lithium-1Hauptgruppe@users.noreply.github.com>
2021-12-11 13:41:51 +0100

    feat(all_test) Added call for db insert function tests

--- tests/all_test.go
+++ tests/all_test.go
@@ -8,7 +8,11 @@ func TestAll(t *testing.T) {
 	// Tests für die find Funktionen der Datenbank
 	t.Run("TestFind", TestFind)
 
+	//Tests für die Eingabefunktionen der Datenbank
+	t.Run("TestInsert", TestInsert)
+
 	// Tests für die Berechnungsvorschrift von Dienstreisen, Pendelwege und IT-Geraeten
 	t.Run("TestComputations", TestComputations)
 	t.Run("TestComputationsEnergie", TestComputationsEnergie)
+
 }
