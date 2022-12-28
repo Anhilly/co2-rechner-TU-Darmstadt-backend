457a2351d4034dc96e7a0b9441ffcdd1f58f39dc
Tobias Wehrle <tobi-wehr@web.de>
2022-12-28 10:49:03 +0100

    feat: Change computations for energy

--- structs/errors.go
+++ structs/errors.go
@@ -78,6 +78,12 @@ var ( // Fehler, die bei Berechnungen auftreten
 
 	// Fehler durch fehlende Implementierung einer Berechnung
 	ErrBerechnungUnbekannt = errors.New("BerechneDienstreisen: Keine Berechnung fuer angegeben ID vorhanden")
+
+	// Fehler durch fehlende Angabe des Versorgers für Jahr
+	ErrStrKeinVersorger = "gebaeudeNormalfall: Gebaude %d hat keinen Versorger für Jahr %d eingetragen"
+
+	// Fehler durch fehlenden Energiefaktor für bestimmtes Jahr und Energieform
+	ErrStrKeinFaktorFuerVertrag = "Für Jahr %d ist kein Faktor für Energie %d und Vertrag %d"
 )
 
 var ( // Fehler die bei der Authentifizierung auftreten
